8a7e1d8797a363ea933f16c9db5c6b33f04751a1
mkc1 <coulism@gmail.com>
2016-01-20 15:36:36 -0500

    connected index

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require( 'express' );
 var app = express();
+var swig = require( 'swig' );
 
 
 app.listen(3000, function(){
@@ -7,11 +8,15 @@ app.listen(3000, function(){
   console.log('server listening 3000 again');
 })
 
-app.use(function(req, res, next){
-  res.send("welcome to the special area");
-  next();
-})
 
 app.use(function(req, res, next){
-  res.send("Welcome!");
+  res.render('index');
 })
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
