a55445176d94203509f759c8e37319ce74efa231
mkc1 <coulism@gmail.com>
2016-01-20 15:54:02 -0500

    added index.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require( 'express' );
 var app = express();
 var swig = require( 'swig' );
-
+var routes = require('./routes/');
 
 app.listen(3000, function(){
   console.log('server listening 3000');
@@ -9,9 +9,12 @@ app.listen(3000, function(){
 })
 
 
-app.use(function(req, res, next){
-  res.render('index');
-})
+// app.use(function(req, res, next){
+//   var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
+
+app.use('/', routes);
 
 app.engine('html', swig.renderFile);
 
